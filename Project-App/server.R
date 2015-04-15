a45ea22c50165363fe51bb6bb1fd0ba7861c6d8b
Jason Parker <jason.thomas.parker@gmail.com>
2015-04-15 18:13:13 -0500

    Created files for shiny app
    Simple shiny app that demonstrates the central limit theorem.

---
+++ Project-App/server.R
@@ -0,0 +1,24 @@
+library(shiny)
+data(airquality)
+
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+        
+        output$mainPlot <- renderPlot({
+                x <- vector()
+                for (i in 1:as.numeric(input$samples)){
+                        x[i] <- mean(sample(airquality$Wind, 
+                                            size = as.numeric(input$observations),
+                                            replace = TRUE))
+                }
+                hist(x, breaks = 10,
+                     xlab = "Average wind speed of sample",
+                     ylab = "Frequency",
+                     main = "Frequency of average wind speed")
+                abline(v = mean(airquality$Wind, col = "darkred"))
+        })
+        
+        output$averageWind <- renderPrint({mean(airquality$Wind)})
+     
+})
---
+++ Project-App/ui.R
@@ -0,0 +1,33 @@
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(
+        pageWithSidebar(
+        
+        # Application title
+        headerPanel("Central Limit Theorem example"),
+        
+        # Inputs
+        sidebarPanel(
+
+                p("Demonstration of CLT using the airquality data set."),
+
+                sliderInput("samples",
+                            label = "Number of samples",
+                            min = 10, max = 50, value = 10,
+                            step = 5),
+                sliderInput("observations",
+                            label = "Number of observations per sample",
+                            min = 5, max = 25, value = 5,
+                            step = 5),
+                
+                h4("Average wind speed for airquality dataset"),
+                verbatimTextOutput("averageWind")
+
+        ),
+          
+        mainPanel(
+                plotOutput("mainPlot")
+        )
+
+))
